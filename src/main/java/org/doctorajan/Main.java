14afde0c1882d4d935297dd8896660db22bcf393
khairulazanni <khairul.azanni@gmail.com>
2026-01-28 22:54:55 +0800

    update lesson 1

--- src/main/java/org/doctorajan/Main.java
+++ src/main/java/org/doctorajan/Main.java
@@ -4,6 +4,44 @@ package org.doctorajan;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        int number = 20;
 
+        if (number > 10) {
+            System.out.println("number is greater than 10");
+        }
+        if (number < 10) {
+            System.out.println("number is less than 10");
+        }
+        if (number == 10) {
+            System.out.println("number is equal to 10");
+        }
+
+        char grade = 'D';
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Good");
+                break;
+            default:
+                System.out.println("Fail you idiot");
+                break;
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Iterations " + i);
+        }
+
+        while (number > 0) {
+            System.out.println("number is " + number);
+            number--;
+        }
+
+        number = 10;
+        do {
+            System.out.println("Do number is " + number);
+            number--;
+        } while (number > 0);
     }
 }
